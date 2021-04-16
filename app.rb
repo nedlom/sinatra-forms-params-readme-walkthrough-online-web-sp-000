31204f09d8492752a6ea2effe1b36077685a2c6a
imperative-scientist-4600 <nelom3@gmail.com>
2021-04-16 18:16:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,8 +9,6 @@ class App < Sinatra::Base
   # Add your post route and action below
   
   post '/food' do 
-    binding.pry
-    params.to_s
     "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 
